20b45b3e9d23ba770310cf7dfcf68ca4bbf7a933
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-27 21:22:45 -0400

    Adding inquirer prompt, corresponding template literals, and working out syntax for completing inquirer code execution.

--- Develop/index.js
+++ Develop/index.js
@@ -2,61 +2,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown.js');
+const { error } = require('console');
 
-// TODO: Create an array of questions for user input
-inquirer
- .prompt([ 
-    {
-
-        type: "input",
-        message:"Please enter your GitHub username to continue.",
-        name: "username",
+// The questions variable contains an array of questions for user input in order to create the values to be written to their README.
+const questions = [ 
     
-    },  
     {   type: "input",
         message: "What is your project titled?",
-        name: "title",
+        name: "title"
     
     },
     {
         type: "input",
         message: "Describe your project.",
         name: "description",
-        default: "N/A",
+        default: "N/A"
     },
     {
         type: "input",
         message: "What steps are necessary to install this project?",
         name: "install",
-        default: "N/A",
+        default: "N/A"
 
     },
     {
         type: "input",
         message: "What are some instructions and examples for use of this project?",
         name: "instructions",
-        default: "N/A",
+        default: "N/A"
 
     },
     {
         type: "input",
         message: "Is there anyone you would like to credit for their involvement in this project?",
         name: "credit",
-        default: "N/A",
+        default: "N/A"
 
     },
     {
         type: "input",
         message: "What are some core features of this project?",
         name: "features",
-        default: "N/A",
+        default: "N/A"
 
     },
     {
         type: "input",
         message: "How, if at all, can others help contribute to this project?",
         name: "contribute",
-        default: "N/A",
+        default: "N/A"
 
     },
     {
@@ -65,24 +59,59 @@ inquirer
         name: "licensing"
 
     },
-    
-])
-.then((responses) =>
+    {
 
-console.log(responses)
-)
+        type: "input",
+        message:"Please enter your GitHub username to continue.",
+        name: "username"
+    
+    },
+    {
+    
+        type: "input",
+        message: "Is there an email address you would like to associate with this project?",
+        name: "email"
+    }
+    
+];
+
+inquirer
+    .prompt(questions)
+    .then((data) => {
+        const readTemplate = markdown ({
+            title: `${data.title}`,
+            description: `${data.description}`,
+            install: `${data.install}`,
+            instructions: `${data.instructions}`,
+            credit: `${data.credit}`,
+            features: `${data.features}`,
+            contribute: `${contribute}`,
+            licensing: `${licensing}`,
+            username: `${username}`,
+            email:`${email}`,
+        })
+    if (error) {
+        console.log('Error! Error! Error!');
+    } else {
+        console.log('alright, alright, alright');
+    }
+    });
+// console.log(responses)
+
 // TODO: Create a function to initialize app
 // function init() {
-//     // inquirer.prompt(questions).then(responses),  {
-//     //     console.log("Question prompts firing off!");
-//     }
+//     inquirer.prompt(questions)
+//     .then((responses) => {
+//         console.log("Question prompts firing off!");
+//         writeToFile('./dist/README.md', generateMarkdown({...responses}));
+//     });
 // }
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-};
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(markdown.join(process.cwd(), fileName), data);
+// };
 
 
 
