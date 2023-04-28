7941ba789b5daf9e3c94d061da31a9280cbf66bb
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-27 22:24:55 -0400

    I've got the prompts working as intended.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const markdown = require('./utils/generateMarkdown.js');
-const { error } = require('console');
+const reqMarkdown = require('./utils/generateMarkdown.js');
+
 
 // The questions variable contains an array of questions for user input in order to create the values to be written to their README.
 const questions = [ 
     
-    {   type: "input",
+    {   
+        type: "input",
         message: "What is your project titled?",
         name: "title"
     
@@ -56,6 +57,7 @@ const questions = [
     {
         type: "checkbox",
         message: "What licensing applies to this project?",
+        choices: ["Unlicensed", "Apache License 2.0", "GPL License/General Public License v3.0", "BSD LIcense, 2-clause", "BSD LIcense, 3-clause", "BSD LIcense, 4-clause"],
         name: "licensing"
 
     },
@@ -71,14 +73,40 @@ const questions = [
         type: "input",
         message: "Is there an email address you would like to associate with this project?",
         name: "email"
-    }
+    },
     
 ];
 
-inquirer
-    .prompt(questions)
+// inquirer
+//     .prompt(questions)
+//     .then((data) => {
+//         console.log("Hi " + questions.username);
+//         const readTemplate = markdown ({
+//             title: `${data.title}`,
+//             description: `${data.description}`,
+//             install: `${data.install}`,
+//             instructions: `${data.instructions}`,
+//             credit: `${data.credit}`,
+//             features: `${data.features}`,
+//             contribute: `${contribute}`,
+//             licensing: `${licensing}`,
+//             username: `${username}`,
+//             email:`${email}`,
+// //         })
+//     fs.appendFile('README.md',readTemplate, (error) => {
+//         if (error) {
+//             console.log('Error! Error! Error!');
+//         } else {
+//             console.log('alright, alright, alright');
+//         }
+//     });
+// console.log(responses)
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
     .then((data) => {
-        const readTemplate = markdown ({
+        const readmeMarkdown = generateMarkdown ({
             title: `${data.title}`,
             description: `${data.description}`,
             install: `${data.install}`,
@@ -89,31 +117,15 @@ inquirer
             licensing: `${licensing}`,
             username: `${username}`,
             email:`${email}`,
+            
         })
-    if (error) {
-        console.log('Error! Error! Error!');
-    } else {
-        console.log('alright, alright, alright');
-    }
+        console.log("Question prompts firing off!");
+    fs.appendFile('./dist/README.md', generateMarkdown({...data}));
     });
-// console.log(responses)
-
-// TODO: Create a function to initialize app
-// function init() {
-//     inquirer.prompt(questions)
-//     .then((responses) => {
-//         console.log("Question prompts firing off!");
-//         writeToFile('./dist/README.md', generateMarkdown({...responses}));
-//     });
-// }
+}
 
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {
-//     return fs.writeFileSync(markdown.join(process.cwd(), fileName), data);
-// };
-
-
-
-// Function call to initialize app
+    
 init();
